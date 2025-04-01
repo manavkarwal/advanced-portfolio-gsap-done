ebe79a977becfd95de1de2004db6ee647270ce0e
Manav karwal <haigamekhelte@gmail.com>
2025-04-01 18:05:22 +0530

    Enable HTTPS

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    https: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  scripts: {
+    build: "VITE_SHOULD_RELOAD=true vite build",
+    preview: "vite preview --https"
+  }
 });
